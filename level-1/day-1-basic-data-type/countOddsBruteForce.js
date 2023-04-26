3a679b9302d855cc27ceb6ee1f80551dcafc0568
frederickpukay <28515879+FVPukay@users.noreply.github.com>
2023-04-25 19:15:15 -0700

    level-1: countOddsBruteForce.js

---
+++ level-1/day-1-basic-data-type/countOddsBruteForce.js
@@ -0,0 +1,23 @@
+// 1523. Count Odd Numbers in an Interval Range
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/?envType=study-plan&id=programming-skills-i
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+  // Brute Force
+  // Time Complexity: O(N)
+  // Space Complexity: O(1)
+  
+  let count = 0;
+
+  for (let i = low; i <= high; i++) {
+      if (i % 2 !== 0) {
+          count++;
+      }
+  }
+
+  return count;
+};
